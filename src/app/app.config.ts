1d6e578509aaea0fc2386bf4229591f0c9ae8c90
Alexander Zhidkov <azhidkov@gmail.com>
2023-12-13 16:42:51 +0800

    add material

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,18 +7,19 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 import { getStorage, provideStorage } from '@angular/fire/storage';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     importProvidersFrom(provideFirebaseApp(() => initializeApp({
-      "projectId": "reflectalplatform",
-      "appId": "1:558440967425:web:1cda7736bd9a4f6157ff00",
-      "storageBucket": "reflectalplatform.appspot.com",
-      "apiKey": "AIzaSyDRGfqmS0-esFs4ItEZMVmaLUVp4DEZNl0",
-      "authDomain": "reflectalplatform.firebaseapp.com",
-      "messagingSenderId": "558440967425",
-      "measurementId": "G-TKQB97G9Q3"
+        "projectId": "reflectalplatform",
+        "appId": "1:558440967425:web:1cda7736bd9a4f6157ff00",
+        "storageBucket": "reflectalplatform.appspot.com",
+        "apiKey": "AIzaSyDRGfqmS0-esFs4ItEZMVmaLUVp4DEZNl0",
+        "authDomain": "reflectalplatform.firebaseapp.com",
+        "messagingSenderId": "558440967425",
+        "measurementId": "G-TKQB97G9Q3"
     }))),
     importProvidersFrom(provideAuth(() => getAuth())),
     importProvidersFrom(provideAnalytics(() => getAnalytics())),
@@ -26,5 +27,6 @@ export const appConfig: ApplicationConfig = {
     importProvidersFrom(provideStorage(() => getStorage())),
     ScreenTrackingService,
     UserTrackingService,
-  ]
+    provideAnimations()
+]
 };
